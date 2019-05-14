ac1d926ba8aed5ccd6250be55b25a6b0f939dcfb
wangyao2221 <wangyao2221@163.com>
2019-05-14 22:16:35 +0800

    'Highest Scoring Word'

--- src/com/wangyao2221/cn/Kata.java
+++ src/com/wangyao2221/cn/Kata.java
@@ -15,4 +15,27 @@ public class Kata {
 
         return length;
     }
+
+    public static String high(String s) {
+        // Your code here...
+        String[] sArr = s.split(" ");
+        String result = sArr[0];
+        int score = 0;
+
+        for (String s1 : sArr) {
+            int temp_score = 0;
+
+            for (int i = 0; i < s1.length(); i++) {
+                temp_score += s1.charAt(i) - 'a';
+            }
+
+            System.out.println(s1 + ":" + temp_score);
+
+            if (temp_score > score){
+                score = temp_score;
+                result = s1;
+            }
+        }
+        return result;
+    }
 }
