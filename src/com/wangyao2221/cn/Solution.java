98750faddbd7ca50ad43e13cf49fc770be7c8ab8
= <=>
2019-05-16 20:22:28 +0800

    'camelCase'

---
+++ src/com/wangyao2221/cn/Solution.java
@@ -0,0 +1,32 @@
+package com.wangyao2221.cn;
+
+public class Solution {
+
+    public static String camelCase(String str) {
+        // your code here
+        String[] strArr = str.split(" ");
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (String s : strArr) {
+            if (s.length() == 0) continue;;
+
+            char ch = s.charAt(0);
+
+            ch = upper(ch);
+
+            stringBuilder.
+                    append(ch).
+                    append(s.substring(1,s.length()));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static char upper(char ch){
+        if (ch > 'Z'){
+            ch = (char) (ch - ('a' - 'A'));
+        }
+
+        return ch;
+    }
+}
