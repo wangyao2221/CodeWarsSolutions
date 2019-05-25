f254079e17839a512aedf4baffe86fd7b6f32d03
wangyao2221 <wangyao2221@163.com>
2019-05-25 11:21:05 +0800

    'Add Decompose Comments'

--- src/com/wangyao2221/cn/Decompose.java
+++ src/com/wangyao2221/cn/Decompose.java
@@ -4,19 +4,26 @@ package com.wangyao2221.cn;
 public class Decompose {
 
     public String decompose(long n) {
-        for (long i = n - 1;i > 1;i--){
-            long squareRem = n * n;
+        // 第一层循环的遍历作为一组数据的最大值
+        for (long i = n - 1; i > 1; i--) {
+            long squareRem = n * n; // n^2
             StringBuilder stringBuilder = new StringBuilder();
 
+            // 从这组的最大值开始遍历
             for (long j = i; j > 0; j--) {
-                if (j * j < squareRem){
+                // 如果j^2 < squareRem，则表示j^2有可能是squareRem的一个加数
+                if (j * j < squareRem) {
                     stringBuilder.insert(0, " " + j);
+                    // 把j^2从squareRem减掉，以便找后续的加数
                     squareRem -= j * j;
-                }else if (j * j == squareRem){
-                    stringBuilder.insert(0,j);
+                    System.out.print(j + " ");
+                } else if (j * j == squareRem) { // 找到一组值
+                    stringBuilder.insert(0, j);
+                    System.out.print(j + " ");
                     return stringBuilder.toString();
                 }
             }
+            System.out.println();
         }
 
         return null;
