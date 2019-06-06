c428243eccaf96868f3e7752326ccf2a0a72739f
wangyao2221 <wangyao2221@163.com>
2019-06-06 22:10:15 +0800

    'Path Finder 一个超时'

--- src/com/wangyao2221/cn/Finder.java
+++ src/com/wangyao2221/cn/Finder.java
@@ -12,12 +12,11 @@ public class Finder {
         int row = mazeArr.length;
         int col = maze.length() / mazeArr.length;
         String[][] matrix = new String[row][col];
-        boolean[][] visit = new boolean[row][col];
         int[][] depth = new int[row][col];
         depth[0][0] = 0;
         Queue<Position> positionQueue = new LinkedList<Position>() {
             {
-                add(new Position(0, 0));
+                offer(new Position(0, 0));
             }
         };
 
@@ -25,29 +24,24 @@ public class Finder {
             matrix[i] = mazeArr[i].split("");
         }
 
+        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
         while (!positionQueue.isEmpty()) {
             Position position = positionQueue.poll();
             int i = position.x;
             int j = position.y;
-            visit[i][j] = true;
-
             if (i == matrix.length - 1 && j == matrix[0].length - 1) return depth[i][j];
+            matrix[i][j] = "W";
 
-            if (i - 1 > 0 && visit[i - 1][j] == false && ".".equals(matrix[i - 1][j])) {
-                positionQueue.add(new Position(i - 1, j));
-                depth[i - 1][j] = depth[i][j] + 1;
-            }
-            if (j - 1 > 0 && visit[i][j - 1] == false  && ".".equals(matrix[i][j - 1])) {
-                positionQueue.add(new Position(i - 1, j));
-                depth[i][j - 1] = depth[i][j] + 1;
-            }
-            if (i + 1 < matrix.length && visit[i + 1][j] == false  && ".".equals(matrix[i + 1][j])) {
-                positionQueue.add(new Position(i + 1, j));
-                depth[i + 1][j] = depth[i][j] + 1;
-            }
-            if (j + 1 < matrix[i].length && visit[i][j + 1] == false && ".".equals(matrix[i][j + 1])) {
-                positionQueue.add(new Position(i, j + 1));
-                depth[i][j + 1] = depth[i][j] + 1;
+            for (int k = 0; k < dir.length; k++) {
+                int posX = i + dir[k][0];
+                int posY = j + dir[k][1];
+
+                if (posX < 0 || posY < 0 || posX >= matrix.length || posY >= matrix[i].length || "W".equals(matrix[posX][posY]))
+                    continue;
+
+                positionQueue.offer(new Position(posX, posY));
+                depth[posX][posY] = depth[i][j] + 1;
             }
         }
 
@@ -62,5 +56,18 @@ public class Finder {
             this.x = x;
             this.y = y;
         }
+    }
+
+    public static void main(String[] args) {
+        String test = "......W..\n" +
+                "..W....W.\n" +
+                "....WW...\n" +
+                ".....WWW.\n" +
+                "...WW..W.\n" +
+                "....W.W..\n" +
+                "WW.W.W..W\n" +
+                "W.W......\n" +
+                "..WW.W.W.";
+        System.out.println(pathFinder(test));
     }
 }
