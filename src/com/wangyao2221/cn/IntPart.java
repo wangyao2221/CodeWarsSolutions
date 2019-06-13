754d7c5581110de6c3faae3d935c59aee95dfcac
wangyao2221 <wangyao2221@163.com>
2019-06-13 21:16:07 +0800

    Maze

---
+++ src/com/wangyao2221/cn/IntPart.java
@@ -0,0 +1,11 @@
+package com.wangyao2221.cn;
+
+// https://www.codewars.com/kata/getting-along-with-integer-partitions/train/java
+public class IntPart {
+	
+  public static String part(long n) {
+      // your code
+      return null;
+  }
+}
+
