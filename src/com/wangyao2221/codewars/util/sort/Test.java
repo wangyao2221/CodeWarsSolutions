453c3998e63c0723a8156c35f5f5afb70da506ef
wangyao2221 <wangyao2221@163.com>
2019-07-03 22:08:55 +0800

    commit

--- src/com/wangyao2221/codewars/util/sort/Test.java
+++ src/com/wangyao2221/codewars/util/sort/Test.java
@@ -2,6 +2,11 @@ package com.wangyao2221.codewars.util.sort;
 
 public class Test {
     public static void main(String[] args) {
+        int[] sourceArray = {5,2,7,3,6,1,4};
+        int[] arr = new HeapSort().sort(sourceArray);
 
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
