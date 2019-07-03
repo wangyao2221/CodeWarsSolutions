12f0023a84b92b7b92893a97f7bb5ee4c60cea74
wangyao2221 <wangyao2221@163.com>
2019-07-03 22:07:51 +0800

    commit

--- src/com/wangyao2221/codewars/util/sort/Test.java
+++ src/com/wangyao2221/codewars/util/sort/Test.java
@@ -2,12 +2,6 @@ package com.wangyao2221.codewars.util.sort;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {4,2,3,4};
-        IArraySort sort = new BubbleSort();
-        sort.sort(arr);
 
-        for (int i : arr) {
-            System.out.println(i);
-        }
     }
 }
